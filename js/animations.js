452a2919da8f77480bb69023afd796965b2b0bd4
eXoriare <karandashko@gmail.com>
2022-02-05 21:38:18 +0500

    Финальный релиз ^-^

--- js/animations.js
+++ js/animations.js
@@ -3,7 +3,7 @@ const INCREASE_NUMBER_ANIMATION_SPEED = 50;
 function increaseNumberAnimationStep (i, element, endNumber) {
   if (i <= endNumber) {
     if (i === endNumber) {
-      element.innerText = i + '+';
+      element.innerText = i + "+";
     } else {
       element.innerText = i;
     }
@@ -17,9 +17,72 @@ function increaseNumberAnimationStep (i, element, endNumber) {
   }
 
 function initIncreaseNumberAnimation() {
-  const element = document.querySelector('.features__clients-count');
+  const element = document.querySelector(".features__clients-count");
   
   increaseNumberAnimationStep(0, element, 5000);
 }
 
-initIncreaseNumberAnimation();
+
+
+document.querySelector("#budget").addEventListener("change", function handleSelectChange(event) {
+  if (event.target.value === "other") {
+    const formContainer = document.createElement("div");
+    formContainer.classList.add("form__group");
+    formContainer.classList.add("form__other-input");
+
+    const input = document.createElement("input");
+    input.placeholder = "Введите ваш вариант";
+    input.type = "text";
+
+    formContainer.appendChild(input);
+    document.querySelector("#form form").insertBefore(formContainer, document.querySelector(".form__submit"));
+  }
+ 
+  const otherInput = document.querySelector('.form__other-input');
+  if (event.target.value !== 'other' && otherInput) {
+  document.querySelector('#form form').removeChild(otherInput);
+  }
+});
+
+
+
+let animationInited = false;
+
+function updateScroll() {
+  if (window.scrollY > 0) {
+    document.querySelector("header").classList.add("header__scrolled")
+  } else {
+    document.querySelector("header").classList.remove("header__scrolled")
+  }
+  
+    // Запуск анимации увеличения числа
+  let windowBottomPosition = window.scrollY + window.innerHeight;
+  let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+  if (windowBottomPosition >= countElementPosition && !animationInited) {
+    animationInited = true;
+    initIncreaseNumberAnimation();
+  }
+}
+
+window.addEventListener("scroll", updateScroll);
+
+
+
+function addSmoothScroll(link) {   // 4.7
+  link.addEventListener("click", onLinkClick);
+}
+
+
+
+function onLinkClick(event) {
+  event.preventDefault();
+
+  document.querySelector(event.target.getAttribute("href")).scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+document.querySelectorAll(`a[href^="#"]`).forEach(link => {addSmoothScroll(link);
+});
+
+addSmoothScroll(document.querySelector('.more-button'));
